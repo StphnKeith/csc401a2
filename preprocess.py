01d6162a4fefda236380603d92ad3e5b22e7f172
stphnkeith <stphnkeith@gmail.com>
2019-03-03 17:12:55 -0500

    added preprocess plan

--- preprocess.py
+++ preprocess.py
@@ -15,6 +15,18 @@ def preprocess(in_sentence, language):
 	out_sentence: (string) the modified sentence
     """
     # TODO: Implement Function
-	
+	# separate sentence-final punctuation
+    # separate commas, colons, semicolons, parentheses, dashes,
+    # mathematical ops (+, -, <, >, =), and quotation marks
+    # add SENTSTART and SENTEND to the end of the sentence
+
+    # when language == 'f' separate the following contractions:
+        # leading l' from concatenated word: l'election => l' election
+        # leading consonant and apostraphe from concatenated word: j'ai => j' ai
+        # leading qu' from concatenated word: qu'on => qu' on
+        # following on or il: puisqu'on => puisqu' on, lorsqu'il => lorsqu' il
+    # any words containing apostraphes not described above can be left as is
+    # the following words should not be separated:
+        # d'abord, d'accord, d'ailleurs, d'habitude
 
     return out_sentence
