638ebc44e8ec1bec9e99a73de5375b737e2c755e
Stephen Keith <stphnkeith@gmail.com>
2019-03-08 04:59:31 -0500

    inconsistent spaces vs tabs

--- preprocess.py
+++ preprocess.py
@@ -5,17 +5,17 @@ def preprocess(in_sentence, language):
     This function preprocesses the input text according to language-specific rules.
     Specifically, we separate contractions according to the source language, convert
     all tokens to lower-case, and separate end-of-sentence punctuation 
-	
-	INPUTS:
-	in_sentence : (string) the original sentence to be processed
-	language	: (string) either 'e' (English) or 'f' (French)
-				   Language of in_sentence
-				   
-	OUTPUT:
-	out_sentence: (string) the modified sentence
+    
+    INPUTS:
+    in_sentence : (string) the original sentence to be processed
+    language    : (string) either 'e' (English) or 'f' (French)
+                   Language of in_sentence
+                   
+    OUTPUT:
+    out_sentence: (string) the modified sentence
     """
     # TODO: Implement Function
-	# separate sentence-final punctuation
+    # separate sentence-final punctuation
     # separate commas, colons, semicolons, parentheses, dashes,
     # mathematical ops (+, -, <, >, =), and quotation marks
 
---
+++ task3.py
@@ -0,0 +1,16 @@
+from perplexity import *
+
+def task3():
+    # For both language models:
+        # Load language model from pickle file
+        # Record into Task3.txt (or print):
+            # Perplexity of /u/cs401/A2_SMT/data/Hansard/Testing/ with:
+                # no smoothing
+                # delta = 0.2
+                # delta = 0.4
+                # delta = 0.6
+                # delta = 0.8
+                # delta = 1.0
+
+if __name__ == "__main__":
+    task3()
