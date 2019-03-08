346c8e663d793ea644450e9d988a5b5eb864f1e8
Stephen Keith <stphnkeith@gmail.com>
2019-03-08 04:54:45 -0500

    .

--- align_ibm1.py
+++ align_ibm1.py
@@ -6,24 +6,24 @@ import os
 
 def align_ibm1(train_dir, num_sentences, max_iter, fn_AM):
     """
-	Implements the training of IBM-1 word alignment algoirthm. 
-	We assume that we are implemented P(foreign|english)
-	
-	INPUTS:
-	train_dir : 	(string) The top-level directory name containing data
-					e.g., '/u/cs401/A2_SMT/data/Hansard/Testing/'
-	num_sentences : (int) the maximum number of training sentences to consider
-	max_iter : 		(int) the maximum number of iterations of the EM algorithm
-	fn_AM : 		(string) the location to save the alignment model
-	
-	OUTPUT:
-	AM :			(dictionary) alignment model structure
-	
-	The dictionary AM is a dictionary of dictionaries where AM['english_word']['foreign_word'] 
-	is the computed expectation that the foreign_word is produced by english_word.
-	
-			AM['house']['maison'] = 0.5
-	"""
+    Implements the training of IBM-1 word alignment algoirthm. 
+    We assume that we are implemented P(foreign|english)
+    
+    INPUTS:
+    train_dir :     (string) The top-level directory name containing data
+                    e.g., '/u/cs401/A2_SMT/data/Hansard/Testing/'
+    num_sentences : (int) the maximum number of training sentences to consider
+    max_iter :      (int) the maximum number of iterations of the EM algorithm
+    fn_AM :         (string) the location to save the alignment model
+    
+    OUTPUT:
+    AM :            (dictionary) alignment model structure
+    
+    The dictionary AM is a dictionary of dictionaries where AM['english_word']['foreign_word'] 
+    is the computed expectation that the foreign_word is produced by english_word.
+    
+            AM['house']['maison'] = 0.5
+    """
     AM = {}
     
     # Read training data
@@ -41,19 +41,19 @@ def align_ibm1(train_dir, num_sentences, max_iter, fn_AM):
 # ------------ Support functions --------------
 def read_hansard(train_dir, num_sentences):
     """
-	Read up to num_sentences from train_dir.
-	
-	INPUTS:
-	train_dir : 	(string) The top-level directory name containing data
-					e.g., '/u/cs401/A2_SMT/data/Hansard/Testing/'
-	num_sentences : (int) the maximum number of training sentences to co
+    Read up to num_sentences from train_dir.
+    
+    INPUTS:
+    train_dir :     (string) The top-level directory name containing data
+                    e.g., '/u/cs401/A2_SMT/data/Hansard/Testing/'
+    num_sentences : (int) the maximum number of training sentences to co
 
-	
-	Make sure to preprocess!
-	Remember that the i^th line in fubar.e corresponds to the i^th line in fubar.f.
-	
-	Make sure to read the files in an aligned manner.
-	"""
+    
+    Make sure to preprocess!
+    Remember that the i^th line in fubar.e corresponds to the i^th line in fubar.f.
+    
+    Make sure to read the files in an aligned manner.
+    """
     """
     # TODO
     # Get starting files from directory:
@@ -150,10 +150,10 @@ def read_hansard(train_dir, num_sentences):
 
 def initialize(eng, fre):
     """
-	Initialize alignment model uniformly.
-	Only set non-zero probabilities where word pairs appear in corresponding sentences.
-	"""
-	# TODO
+    Initialize alignment model uniformly.
+    Only set non-zero probabilities where word pairs appear in corresponding sentences.
+    """
+    # TODO
     # Setup keys of AM
     # For each sentence pair:
         # For each word in english:
@@ -196,10 +196,10 @@ def initialize(eng, fre):
     
 def em_step(t, eng, fre):
     """
-	One step in the EM algorithm.
-	Follows the pseudo-code given in the tutorial slides.
-	"""
-	# TODO
+    One step in the EM algorithm.
+    Follows the pseudo-code given in the tutorial slides.
+    """
+    # TODO
     # Lecture Steps:
         # 1. Make a table of P(f|e) for all possible pairs of f and e, prob_tab
         # 2. Make a grid where each sentence pair is a row and each possible
